7fdcbccc44761ab33a441a4cf471a13c6d3a8b91
Jen <jen.schwabe2@gmail.com>
2021-11-06 15:50:22 -0500

    Worked on the post formatting, etc.

--- insta-script.js
+++ insta-script.js
@@ -1,23 +1,22 @@
-
 // chooses and displays the correct draft
 function f(number) {
-    console.log("open draft " + number);
+  console.log("open draft " + number);
 }
 
-function toggleComments() {
+function toggleComments(post) {
   console.log("toggling comments visibility");
-  var comments = document.getElementsByClassName("comments");
-  var button = document.getElementById("load-comments")
+  var commentsname = "comments" + post;
+  var buttonname = "load-comments" + post;
+  var comments = document.getElementById(commentsname);
+  var button = document.getElementById(buttonname)
   if (button.innerHTML === "Show comments") {
     button.innerHTML = "Hide comments";
   } else {
     button.innerHTML = "Show comments";
   }
-  for (var i = 0; i < comments.length; i++) {
-    if (comments[i].style.display === "block") {
-      comments[i].style.display = "none";
-    } else {
-      comments[i].style.display = "block";
-    }
+  if (comments.style.display === "block") {
+    comments.style.display = "none";
+  } else {
+    comments.style.display = "block";
   }
 }
