441b71f85666f12c60a65dbd084edcb1c5204095
Noopur Shanbhogue <noopurs2@illinois.edu>
2022-01-22 15:44:00 -0600

    made images popup

--- instabook-script.js
+++ instabook-script.js
@@ -41,3 +41,47 @@ function toggleDialogue() {
     dialogue.style.display = "block";
   }
 }
+
+//modal code for dog
+var modal = document.getElementById("dogmodal");
+var img = document.getElementById("dog");
+var modalImg = document.getElementById("img02");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+var span = document.getElementsByClassName("close2")[0];
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+//modal code for image pop up: school 
+var modal = document.getElementById("schoolmodal");
+var img = document.getElementById("school");
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+//modal code for image pop up: car 
+var modal = document.getElementById("carmodal");
+var img = document.getElementById("car");
+var modalImg = document.getElementById("img03");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close3")[0];
+span.onclick = function() {
+  modal.style.display = "none";
+}
