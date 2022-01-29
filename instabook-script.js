7101e4f72e7d4603057cdfd80a5f8d5687538e9d
Jen <jen.schwabe2@gmail.com>
2022-01-29 15:43:16 -0600

    Coded in dialogue pop-up

--- instabook-script.js
+++ instabook-script.js
@@ -3,41 +3,66 @@ function f(number) {
   console.log("open draft " + number);
 }
 
+var dialogueBox = document.getElementById("dialogue");
+var dialogueDiv = document.getElementById("dialogueBox");
+var dialogueShown = false;
+var pageCover = document.getElementById("pageCover");
+
 function toggleComments(post) {
   console.log("toggling comments visibility");
-  var commentsname = "comments" + post;
-  var buttonname = "load-comments" + post;
-  var comments = document.getElementById(commentsname);
-  var button = document.getElementById(buttonname)
-  if (button.innerHTML === "Show comments") {
-    button.innerHTML = "Hide comments";
-  } else {
-    button.innerHTML = "Show comments";
-  }
-  if (comments.style.display === "block") {
-    comments.style.display = "none";
-  } else {
-    comments.style.display = "block";
-  }
-}
-
-function progressDialogue() {
-  var dialogue = document.getElementById("dialogue");
-  if (dialogue.textContent === "Clicked to progress dialogue") {
-    dialogue.textContent = "Progressed dialogue again";
-  } else {
-    dialogue.textContent = "Clicked to progress dialogue";
+  if (!dialogueShown) {
+    var commentsname = "comments" + post;
+    var buttonname = "load-comments" + post;
+    var comments = document.getElementById(commentsname);
+    var button = document.getElementById(buttonname)
+    if (button.innerHTML === "Show comments") {
+      button.innerHTML = "Hide comments";
+    } else {
+      button.innerHTML = "Show comments";
+    }
+    if (comments.style.display === "block") {
+      comments.style.display = "none";
+    } else {
+      comments.style.display = "block";
+    }
   }
 }
 
-function toggleDialogue() {
-  console.log("Clicked to toggle dialogue");
-  var dialogue = document.getElementById("dialogueBox");
-  var dialogueactual = document.getElementById("dialogue");
-  dialogueactual.textContent = "Alma says hi";
-  if (dialogue.style.display === "block") {
-    dialogue.style.display = "none";
+function progressDialogue(dialogueNumber) {
+  console.log("Progressed dialogue");
+  var currentDialogue = dialogueBox.innerHTML;
+  var dialogueIndex = dialogue[dialogueNumber].findIndex((element) => element === currentDialogue);
+  if (dialogue[dialogueNumber].length - 1 === dialogueIndex) {
+    console.log("Hiding dialogue");
+    dialogueDiv.style.display = "none";
+    dialogueShown = false;
+    pageCover.style.display = "none";
   } else {
-    dialogue.style.display = "block";
+    dialogueBox.innerHTML = dialogue[dialogueNumber][dialogueIndex + 1];
   }
 }
+
+function toggleDialogue(dialogueNumber) {
+  console.log("Clicked to toggle dialogue 0");
+  dialogueShown = true;
+  pageCover.style = "block";
+  dialogueBox.innerHTML = initialDialogue[dialogueNumber];
+  if (dialogueDiv.style.display === "none") {
+    dialogueDiv.style.display = "block";
+  }
+}
+
+var dialogue = [
+  ["Let's check the Doughmino's Pizza for fraud! I heard there might be some identity issues there."],
+  ["Let's see if we can find any information about the employees."],
+  ["This name could be useful. Let’s see what we can find out about this person."],
+  ["Perhaps we can find this person’s account because we know this business is ",
+    "involved with some shady stuff. Let’s use our special search engine back at the base."
+  ],
+  ["All right, let's see what we can find out about __!"],
+  ["It looks like we might be able to find some valuable information here."],
+  ["I wonder if we can get into this email to find evidence of shady doings."],
+  ["Darn, I wonder how we can get the answers to these questions?"]
+];
+
+var initialDialogue = ["Perhaps we can find this person’s account because we know this business is "];
