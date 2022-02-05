4b8bda476202c520c4d9cbffd255ae4852f64d1b
Jen <jen.schwabe2@gmail.com>
2022-02-05 15:35:36 -0600

    Made some tweaks to the dialogue box code. Made the skeleton of the LinkedIn page.

--- instabook-script.js
+++ instabook-script.js
@@ -28,41 +28,33 @@ function toggleComments(post) {
   }
 }
 
-function progressDialogue(dialogueNumber) {
-  console.log("Progressed dialogue");
-  var currentDialogue = dialogueBox.innerHTML;
-  var dialogueIndex = dialogue[dialogueNumber].findIndex((element) => element === currentDialogue);
-  if (dialogue[dialogueNumber].length - 1 === dialogueIndex) {
-    console.log("Hiding dialogue");
-    dialogueDiv.style.display = "none";
+function toggleDialogue(dialogueNumber) {
+  console.log("Clicked to toggle dialogue " + dialogueNumber);
+  if (dialogueShown === true) {
     dialogueShown = false;
     pageCover.style.display = "none";
+    dialogueDiv.style.display = "none";
+    console.log("Hiding dialogue");
   } else {
-    dialogueBox.innerHTML = dialogue[dialogueNumber][dialogueIndex + 1];
-  }
-}
-
-function toggleDialogue(dialogueNumber) {
-  console.log("Clicked to toggle dialogue 0");
-  dialogueShown = true;
-  pageCover.style = "block";
-  dialogueBox.innerHTML = initialDialogue[dialogueNumber];
-  if (dialogueDiv.style.display === "none") {
+    dialogueShown = true;
+    pageCover.style = "block";
+    dialogueBox.innerHTML = dialogue[dialogueNumber];
+    console.log(dialogue[dialogueNumber]);
     dialogueDiv.style.display = "block";
   }
 }
 
 var dialogue = [
-  ["Let's check the Doughmino's Pizza for fraud! I heard there might be some identity issues there."],
-  ["Let's see if we can find any information about the employees."],
-  ["This name could be useful. Let’s see what we can find out about this person."],
-  ["Perhaps we can find this person’s account because we know this business is ",
-    "involved with some shady stuff. Let’s use our special search engine back at the base."
-  ],
-  ["All right, let's see what we can find out about __!"],
-  ["It looks like we might be able to find some valuable information here."],
-  ["I wonder if we can get into this email to find evidence of shady doings."],
-  ["Darn, I wonder how we can get the answers to these questions?"]
+  "We don't need to look here right now.",
+  "Let's check the Doughmino's Pizza for fraud! I heard there might be some identity issues there.",
+  "Let's see if we can find any information about the employees.",
+  "This name could be useful. Let’s see what we can find out about this person.",
+  "Perhaps we can find this person’s account because we know this business is " +
+    "involved with some shady stuff. Let’s use our special search engine back at the base.",
+  "All right, let's see what we can find out about __!",
+  "It looks like we might be able to find some valuable information here.",
+  "It looks like John Smith's email is suspiciousperson@email.com. Let's use this information to try" +
+  " to get into his email to find evidence of shading doings.", 
+  "I wonder if we can get into this email to find evidence of shady doings.",
+  "Darn, I wonder how we can get the answers to these questions?"
 ];
-
-var initialDialogue = ["Perhaps we can find this person’s account because we know this business is "];
